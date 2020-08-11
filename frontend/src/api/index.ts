f4041874f81d0178286709392b2a3928db5aa392
小舟从此逝 <kopor_oo@qq.com>
2020-08-12 00:45:52 +0800

    ..

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -5,7 +5,7 @@ import {
 
 
 const get_article=(id=1)=>instance.get('article/'+id)
-const add_article=(c="")=> instance.post("article",c)
+const add_article=(c={})=> instance.post("article",c)
 
 export {
     get_article,
